91ac1f70011be7aaf386930058d371c7e387d95c
Emmet Ryan <emmetryan7@gmail.com>
2021-05-06 14:47:02 -0400

    Changed onload function to make table work

--- public/script.js
+++ public/script.js
@@ -1,30 +1,27 @@
-async function Records(){
-    console.log('data request');
-<<<<<<< Updated upstream
-    const recordsRequest = await fetch('/api/allrecords');
-    const recordsData = await recordsRequest.json();
-    return recordsData;
+async function Records() {
+  console.log('data request');
+  const recordsRequest = await fetch('/api/allrecords');
+  const recordsData = await recordsRequest.json();
+  return recordsData;
 }
 async function Calendar() {
-    const apif = await fetch('/api/allrecords');
-=======
-    const recordsRequest = await fetch('/api/properties');
-    const recordsData = await recordsRequest.json();
-    return recordsData;
+  const apif = await fetch('/api/allrecords');
+  const recordsRequest = await fetch('/api/properties');
+  const recordsData = await recordsRequest.json();
+  return recordsData;
 }
 async function calendarTable() {
-    const apif = await fetch('/api/properties');
->>>>>>> Stashed changes
-    const recordsArray = await apif.json();
-    const y = document.querySelector('.target'); 
-    recordsArray.forEach((c) => {
-        const CVar = document.createElement('tr')
-        CVar.innerHTML =`
-            <td>${c.calendar_id}</td>
-            <td>${c.listing_id}</td>
-            <td>${c.stay_date}</td>
+  const apif = await fetch('/api/properties');
+  const recordsArray = await apif.json();
+  const y = document.querySelector('.target');
+  recordsArray.forEach((c) => {
+    const CVar = document.createElement('tr');
+    CVar.innerHTML = `
+            <td>${c.property_id}</td>
+            <td>${c.room_type}</td>
+            <td>${c.bathrooms}</td>
         `;
-        y.append(CVar)
-    });
+    y.append(CVar);
+  });
 }
-window.onload = Calendar();
+window.onload = calendarTable();
